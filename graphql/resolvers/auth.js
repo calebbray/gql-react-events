bdf20291f5a5751182b9c94f4939f08100bf9624
Caleb Bray <caleb.bray@economicmodeling.com>
2019-03-06 16:42:15 -0800

    beginning login functionality

--- graphql/resolvers/auth.js
+++ graphql/resolvers/auth.js
@@ -21,5 +21,19 @@ module.exports = {
     } catch (err) {
       throw err;
     }
+  },
+
+  async login({ email, password }) {
+    try {
+      const user = await User.findOne({ email });
+      const hashPass = await bcrypt.hash(password, 12);
+      // if (!user || user.password !== hashPass) {
+      //   throw new Error('Unable to login');
+      // }
+
+      console.log(hashPass, user.password);
+    } catch (err) {
+      throw err;
+    }
   }
 };
